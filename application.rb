85f1d2788e1c01495d47427fe6c7ec6b714dc68b
Ben Langfeld <ben@langfeld.me>
2012-05-14 18:16:25 +0200

    De-lint

--- application.rb
+++ application.rb
@@ -1,17 +1,11 @@
-%w(rubygems awesome_print sinatra).each{|lib| require lib}
+%w(rubygems awesome_print sinatra).each { |lib| require lib }
 
 configure do
-	set :views, "#{File.dirname(__FILE__)}/views"
+  set :views, "#{File.dirname(__FILE__)}/views"
 end
 
-
-helpers do
-	# add your helpers here
-end
-
-# root page
 get '/' do
-  erb :coming_soon,{:layout => :special}
+  erb :coming_soon, :layout => :special
 end
 
 get '/about' do 
@@ -19,18 +13,17 @@ get '/about' do
   @page = "about"
   erb :about 
 end
-# spec page
+
 get '/spec' do
-	@title = "Rayo Messaging Protocol Specification"
-	@page = "spec"
-	erb :spec
+  @title = "Rayo Messaging Protocol Specification"
+  @page = "spec"
+  erb :spec
 end
 
-# who's using it page
 get '/who' do
-	@title = "Rayo Messaging Protocol - Who's using it"
-	@page = "who"
-	erb :who
+  @title = "Rayo Messaging Protocol - Who's using it"
+  @page = "who"
+  erb :who
 end
 
 get '/xep' do
